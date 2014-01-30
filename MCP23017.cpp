08a00972095b064b3129e0876713d7f393a7a37c
Marc-Andre Ferland <madrang@gmail.com>
2014-01-29 21:29:15 -0500

    Code formating changes.

--- MCP23017.cpp
+++ MCP23017.cpp
@@ -83,12 +83,13 @@ void MCP23017::pinMode(uint8_t p, uint8_t d) {
   uint8_t iodiraddr;
 
   // only 16 bits!
-  if (p > 15)
+  if (p > 15) {
     return;
+  }
 
-  if (p < 8)
+  if (p < 8) {
     iodiraddr = MCP23017_IODIRA;
-  else {
+  } else {
     iodiraddr = MCP23017_IODIRB;
     p -= 8;
   }
@@ -146,8 +147,9 @@ void MCP23017::digitalWrite(uint8_t p, uint8_t d) {
   uint8_t gpioaddr, olataddr;
 
   // only 16 bits!
-  if (p > 15)
+  if (p > 15) {
     return;
+  }
 
   if (p < 8) {
     olataddr = MCP23017_OLATA;
@@ -160,7 +162,7 @@ void MCP23017::digitalWrite(uint8_t p, uint8_t d) {
 
   // read the current GPIO output latches
   Wire.beginTransmission(i2caddr);
-  wiresend(olataddr);	
+  wiresend(olataddr);
   Wire.endTransmission();
   
   Wire.requestFrom(i2caddr, 1);
@@ -185,8 +187,9 @@ void MCP23017::pullUp(uint8_t p, uint8_t d) {
   uint8_t gppuaddr;
 
   // only 16 bits!
-  if (p > 15)
+  if (p > 15) {
     return;
+  }
 
   if (p < 8)
     gppuaddr = MCP23017_GPPUA;
@@ -194,7 +197,6 @@ void MCP23017::pullUp(uint8_t p, uint8_t d) {
     gppuaddr = MCP23017_GPPUB;
     p -= 8;
   }
-
 
   // read the current pullup resistor set
   Wire.beginTransmission(i2caddr);
@@ -222,12 +224,13 @@ uint8_t MCP23017::digitalRead(uint8_t p) {
   uint8_t gpioaddr;
 
   // only 16 bits!
-  if (p > 15)
+  if (p > 15) {
     return 0;
+  }
 
-  if (p < 8)
+  if (p < 8) {
     gpioaddr = MCP23017_GPIOA;
-  else {
+  } else {
     gpioaddr = MCP23017_GPIOB;
     p -= 8;
   }
